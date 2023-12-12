15fb503a14227b259c1d58c59bcf6df160fbabd1
kambizvb6 <kambizvb6@gmail.com>
2023-12-12 16:06:34 -0600

    Initial Commit

---
+++ DataModel/DataModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataModelApp.swift
+//  DataModel
+//
+//  Created by Kambiz on 2023-12-12.
+//
+
+import SwiftUI
+
+@main
+struct DataModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
