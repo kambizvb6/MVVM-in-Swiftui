cb5ba6525c467376654865fd16898462ff9c3772
Kambiz Dehghani <kambizvb6@gmail.com>
2024-01-16 14:45:15 -0600

    Add UserDetailsView

---
+++ DataModel/UserDetails/View/UserDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailsView.swift
+//  DataModel
+//
+//  Created by Kambiz on 2024-01-16.
+//
+
+import SwiftUI
+
+struct UserDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailsView()
+}
